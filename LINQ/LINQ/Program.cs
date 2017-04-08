2cb877642054954dde8c08c0a5c07d3048f0cc95
N <N@MILANA_PC>
2017-04-08 10:23:31 +0300

    starting

---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Порезанов Николай. Д.З. №4.
+/*Создайте 4 сущности: User, UserInfo, Role, Group
+User
+int Id !
+int GroupId
+string Login
+--------------
+UserInfo
+int UserId
+string Name
+int Age
+double Salary
+Group
+int Id !
+int RoleId
+string Name
+-----------------
+Role
+int Id !
+string Name
+Напишите следующие запросы
+a. Выведите все группы и количество пользователей в этих группах
+b. Выведите все группы и количество пользователей в этих группах, возраст которых больше 25
+c. Выведите все группы и количество пользователей в этих группах, возраст которых больше 25 и роль которых, например, администратор
+d. Выведите все группы, при этом в каждой группе нужно сгруппировать всех пользователей по ролям*/
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
